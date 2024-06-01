7c2a4acb3cf12d49edb07b605889536f0d6bf85a
skarack <skarack@gmail.com>
2024-06-01 00:28:18 -0400

    feat: create window

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+using var game = new Tetris(800, 600, "Tetris");
+game.Run();
+
+public class Tetris : GameWindow
+{
+    public Tetris(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title })
+    { 
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+
+        base.OnUpdateFrame(e);
+    }
+}
