d1acb04364cd290f6408dfc1ced2a5747f5d4065
skarack <skarack@gmail.com>
2024-06-13 22:27:06 -0400

    feat: tblock rotation

--- blocks/IBlock.cs
+++ blocks/IBlock.cs
@@ -6,6 +6,6 @@ class IBlock : Block
     public IBlock()
     {
         this.Representations.Enqueue(new ([(0, 0), (0, 1), (0, 2), (0, 3)], 1, 4));
-        this.Representations.Enqueue(new ([(0, 0), (1, 0), (2, 0), (3, 0)], 1, 4));
+        this.Representations.Enqueue(new ([(0, 0), (1, 0), (2, 0), (3, 0)], 4, 1));
     }
 }
